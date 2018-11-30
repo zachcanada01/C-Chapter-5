37576363679fd8d2c6bb849aef1129a42d12fe81
Zach <zachcanada23@gmail.com>
2018-11-30 13:06:32 -0600

    Tipping

--- ValidID/ValidID/Program.cs
+++ ValidID/ValidID/Program.cs
@@ -3,13 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using static System.Console;
 namespace ValidID
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int idNum;
+            string input;
+            const int LOW = 1000;
+            const int HIGH = 9999;
+
+            Write("Enter an ID number");
+            input = ReadLine();
+            idNum = Convert.ToInt32(input);
+
+            while (idNum < LOW || idNum > HIGH)
+            {
+                WriteLine(idNum + " is an invalid ID number ");
+                Write("ID numbers must be between " + LOW + " and " + HIGH);
+                Write(" Enter an ID number");
+                input = ReadLine();
+                idNum = Convert.ToInt32(input);
+            }
+            WriteLine("ID number " + idNum + " is valid");
         }
     }
 }
