fa357056f9f9652ab3135c5e947bd0f2d1167eaa
Zach <zachcanada23@gmail.com>
2018-12-06 12:55:08 -0600

    Added home sales

--- HomeSales/HomeSales/Program.cs
+++ HomeSales/HomeSales/Program.cs
@@ -10,13 +10,63 @@ namespace HomeSales
     {
         static void Main(string[] args)
         {
-            double danielleTotal;
-            double edwardTotal;
-            double francisTotal;
-
-            WriteLine("Please enter your first name initial.");
-
+            double danielleTotal = 0;
+            double edwardTotal = 0;
+            double francisTotal = 0;
+            string userInput;
+            int userSale;
+            string userString;
+            int count = 0;
           
+            do
+            {
+                WriteLine("Please enter your first name initial.");
+                userInput = ReadLine();
+             if (userInput == "d")
+                {
+                    WriteLine("Valid");
+                    WriteLine("Danielle please enter the amount sold, enter z when done.");
+                    userString = ReadLine();
+                    userSale = Convert.ToInt32(userString);
+
+                    count++;
+
+                    danielleTotal = danielleTotal + userSale;
+                }
+                else if (userInput == "e")
+                {
+                    WriteLine("Valid");
+                    WriteLine("Edward please enter the amount sold, enter z when done.");
+                    userString = ReadLine();
+                    userSale = Convert.ToInt32(userString);
+
+                    count++;
+
+                    edwardTotal = edwardTotal + userSale;
+                }
+                else if (userInput == "f")
+                {
+                    WriteLine("Valid");
+                    WriteLine("Francis please enter the amount sold, enter z when done.");
+                    userString = ReadLine();
+                    userSale = Convert.ToInt32(userString);
+
+                    count++;
+
+                    francisTotal = francisTotal + userSale;
+                }
+
+
+                else {
+                    WriteLine("Invalid");
+                }
+                
+                
+            } while(userInput != "z");
+
+            WriteLine("Danielle sold " + danielleTotal + " houses.");
+            WriteLine("Edward sold " + edwardTotal + " house.");
+            WriteLine("Francis sold " + francisTotal + " houses.");
         }
     }
 }
